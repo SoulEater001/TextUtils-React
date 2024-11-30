d37096da3202988c9ad4397aeb27b0afd036b892
SoulEater001 <dshivam698@gmail.com>
2024-12-01 02:15:46 +0530

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 // Components should carry Camel case
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -37,14 +43,23 @@ function App() {
 
   return (
     <>
+      {/* <Router> */}
       <Navbar title="TextUtiles" aboutText="About Us" mode={mode} toggleMode={toggleMode} />
       {/* <Navbar /> */}
       <Alert alert={alert} mode={mode} />
 
+
       <div className="container my-3">
         <TextForm heading="Enter text to analyze" mode={mode} showAlert={showAlert} />
-        {/* <About /> */}
+        {/* <Routes> */}
+        {/* <Route path="/about" element={<About />}> */}
+        {/* </Route> */}
+        {/* <Route path="/" element={<TextForm heading="Enter text to analyze" mode={mode} showAlert={showAlert} />}> */}
+        {/* </Route> */}
+        {/* </Routes> */}
       </div>
+      {/* </Router> */}
+
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+// import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -27,9 +28,9 @@ export default function Navbar(props) {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              {/* <Link className="nav-link" to="/about">
                 {props.aboutText}
-              </a>
+              </Link> */}
             </li>
           </ul>
           {/* <form className="d-flex" role="search">
